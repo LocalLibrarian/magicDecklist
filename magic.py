e810705756f123eb37c6a209867c11a4cc387a33
Travis Shirley <44935136+LocalLibrarian@users.noreply.github.com>
2023-03-05 17:14:25 -0500

    Update magic.py
    Fixed some typos, added more code documentation

--- magic.py
+++ magic.py
@@ -20,6 +20,7 @@ PAGEWAIT = 2 #future support for changing time spent waiting for webpages to loa
 SEP = "~" #future support for changing seperator for database items
 SCALE = 1 #future support for scaling window size
 
+#Defines data attributes common to cards, as well as some useful string formatting
 class card:
     def __init__(self, name, foil, url, price, mrktPrice, type, rarity, category, place):
         self.name = name
@@ -31,11 +32,16 @@ class card:
         self.rarity = rarity
         self.category = category
         self.place = place
+        
+    #Returns good debug string
     def getString(self):
         return self.name + ": priced at " + self.price + ", market price is " + self.mrktPrice + ", of type: " + self.type + ", of rarity: " + self.rarity
+    
+    #Returns string used for saving to database
     def getRaw(self):
         return self.name + "~" + str(self.foil) + "~" + self.url + "~" + self.price + "~" + self.mrktPrice + "~" + self.type + "~" + self.rarity + "~" + self.category + "~" + str(self.place)
 
+#Loads database info from file into listbox in GUI
 def loadDatabase():
     global fileName
     global deckDisplay
@@ -44,10 +50,11 @@ def loadDatabase():
     updated = file.readline()
     updated = datetime.strptime(updated, "%Y-%m-%d %H:%M:%S\n")
     lastUpdated = tk.Label(main, text = "Oldest database item is from " + updated.ctime() + ".").grid(row = 0, column = 0)
+    #Reads line-by-line and adds card names to listbox
     for line in file:
         card = line.split("~")
         print("")
-        print("Read card: ", end = "")
+        print("Read card: ", end = "") #Debug function
         i = 0
         for part in card:
             i += 1
@@ -56,19 +63,22 @@ def loadDatabase():
             print(part, end = ", ")
     file.close()
     
+#Currently just updates database date line, in future will actually update prices from TCGPlayer
 def updateDatabase():
     global fileName
     file = open(fileName, "w")
     file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
     file.close()
-    
+
+#Scrapes image for card selected from TCGPlayer and displays in GUI
 def genImage():
     global deckDisplay
     global fileName
     name = deckDisplay.get(deckDisplay.curselection(), deckDisplay.curselection())[0]
-    print("Generating image for card: " + name)
+    print("Generating image for card: " + name) #Debug function
     file = open(fileName, "r")
     file.readline()
+    #Finds card selected in database file
     for line in file:
         card = line.split("~")
         i = 0
@@ -79,22 +89,24 @@ def genImage():
                 found = True
             if i == 3 and found:
                 url = part
-                print("Getting image from url: " + url)
+                print("Getting image from url: " + url) #Debug function
                 driver = webdriver.Chrome()
                 driver.get(url)
                 driver.maximize_window()
-                time.sleep(PAGEWAIT)
+                time.sleep(PAGEWAIT) #Necessary to let page load
                 img = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/section[2]/section/div/div[2]/section[1]/section/div/div/div/div/div/div/img")   
+                #Saves screenshot of card image to file for display
                 with open('card.png', 'wb') as image:
                     image.write(img.screenshot_as_png)
                 pic = Image.open("card.png")
-                test = ImageTk.PhotoImage(pic)
-                label1 = tk.Label(image=test)
-                label1.image = test
-                label1.grid(row = 2, column = 1)
+                picture = ImageTk.PhotoImage(pic)
+                cardPic = tk.Label(image=picture)
+                cardPic.image = picture
+                cardPic.grid(row = 2, column = 1)
                 break
     file.close()
     
+#Adds card from TCGPlayer URL
 def addCard():
     global fileName
     global cardLink
@@ -102,9 +114,10 @@ def addCard():
     search = webdriver.Chrome()
     search.get(cardLink.get())
     time.sleep(PAGEWAIT) #Necessary to let page load
+    #Scrapes name, price, market price, card type, and rarity from webpage
     name = search.find_element(By.XPATH, "/html/body/div[2]/div/div/section[2]/section/div/div[2]/div/h1")
     name = name.get_attribute("innerHTML")
-    foil = False
+    foil = False #temp, will allow specifying in future
     price = search.find_element(By.XPATH, "/html/body/div[2]/div/div/section[2]/section/div/div[2]/section[2]/section[1]/div/section[2]/span")
     price = price.get_attribute("innerHTML")
     mrktPrice = search.find_element(By.XPATH, "/html/body/div[2]/div/div/section[2]/section/div/div[2]/section[3]/div/section[1]/table/tr[2]/td[2]/span")
@@ -113,15 +126,17 @@ def addCard():
     type = type.get_attribute("innerHTML")
     rarity = search.find_element(By.XPATH, "/html/body/div[2]/div/div/section[2]/section/div/div[2]/section[2]/section[3]/div/div/div/ul/li[1]/span")
     rarity = rarity.get_attribute("innerHTML")
-    category = "Cards"
-    place = 0
+    category = "Cards" #Temp, will allow specifying in future
+    place = 0 #Temp, will allow specifying in future
     new = card(name, foil, cardLink.get(), price, mrktPrice, type, rarity, category, place)
-    print("Added card: " + new.getString())
+    print("Added card: " + new.getString()) #Debug function
+    #Writes card to database file
     file = open(fileName, "a")
     file.write(new.getRaw() + "\n")
     file.close()
-    deckDisplay.insert(tk.END, new.name)
+    deckDisplay.insert(tk.END, new.name) #Adds card to listbox
 
+#Deletes card from listbox and database file. Only deleted based on name currently
 def delCard():
     global deckDisplay
     global fileName
@@ -129,6 +144,7 @@ def delCard():
     file = open(fileName, "r")
     tempFile = open("temp.txt", "w")
     lineNum = 0
+    #Searches for card with selected name in database and writes all others to temp file
     for line in file:
         lineNum += 1
         card = line.split(SEP)
@@ -140,22 +156,26 @@ def delCard():
     file.close()
     file = open(fileName, "w")
     tempFile = open("temp.txt", "r")
+    #Rewrites temp file back to main file without deleted card
     for line in tempFile:
         file.write(line)
     tempFile.close()
     file.close()
-    deckDisplay.delete(deckDisplay.curselection()[0])
+    deckDisplay.delete(deckDisplay.curselection()[0]) #Deletes selected card from listbox
 
+#Creates new .txt file so user doesn't have to do it manually
 def createDatabase():
     name = simpledialog.askstring(title="Database Name", prompt="Enter the name for the new database:")
     file = open(name + ".txt", "w")
     file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
     file.close()
 
+#Main entry point for program
 main = tk.Tk()
-main.geometry("1400x700")
-main.title("Mabic")
+main.geometry("1400x700") #Will be scaled in future via a setting
+main.title("Magic")
 menu = tk.Menu(main)
+#Top menu of program
 menu.add_command(label = "Create New Database", command = createDatabase)
 menu.add_command(label = "Load Database File", command = loadDatabase)
 menu.add_command(label = "Update Database Prices", command = updateDatabase)
