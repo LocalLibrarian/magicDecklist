61744ea97774020446aee0728135a45bf79714da
Travis Shirley <44935136+LocalLibrarian@users.noreply.github.com>
2023-04-12 21:31:56 -0400

    Update magic.py
    - added full settings menu
    - added settings saving
    - implemented proper UI scaling
    - can now delete individual counts of cards rather than all instances

--- magic.py
+++ magic.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from PIL import ImageTk, Image
 from tkinter import simpledialog
+import tkinter.font as font
 
 """
 Database Format:
@@ -26,9 +27,11 @@ quantity of card (int)
 Category is defaulted to "Cards" with placement at the bottom of the category.
 """
 
-global PAGEWAIT 
-PAGEWAIT = 2 #future support for changing time spent waiting for webpages to load 
-SCALE = 1 #future support for scaling window size
+#default settings definition
+global PAGEWAIT
+global SCALE
+PAGEWAIT = 2 #setting for how long to wait for webpages to load
+SCALE = 1 #scales some UI elements
 
 #Defines data attributes common to cards, as well as some useful string formatting
 class card:
@@ -53,6 +56,16 @@ class card:
         file = open(fileName, "a")
         for attribute, value in vars(self).items():
             file.write(str(value) + "\n")
+        file.close()
+
+#Loads settings from file
+def loadSettings():
+    if os.path.exists('settings.txt'):
+        global PAGEWAIT
+        global SCALE
+        file = open('settings.txt')
+        PAGEWAIT = int(file.readline())
+        SCALE = float(file.readline())
         file.close()
 
 #Loads database info from file into listbox in GUI
@@ -124,7 +137,8 @@ def updateDatabase():
 
 #Scrapes image for card selected from TCGPlayer and displays in GUI
 def genImage():
-    global PAGEWAIT 
+    global PAGEWAIT
+    global SCALE
     global deckDisplay
     global cards
     card = cards[deckDisplay.curselection()[0]]
@@ -138,7 +152,8 @@ def genImage():
     with open('card.png', 'wb') as image:
         image.write(img.screenshot_as_png)
     pic = Image.open("card.png")
-    picture = ImageTk.PhotoImage(pic)
+    resized_pic = pic.resize((int(round(313 * SCALE)), int(round(437 * SCALE))), Image.ANTIALIAS)
+    picture = ImageTk.PhotoImage(resized_pic)
     cardPic = tk.Label(image=picture)
     cardPic.image = picture
     cardPic.grid(row = 2, column = 1)
@@ -181,15 +196,13 @@ def delCard():
     global cards
     card = cards[deckDisplay.curselection()[0]]
     card.quantity -= 1
+    file = open(fileName, "r")
+    tempFile = open("temp.txt", "w")
+    name = card.name
+    rpts = 0
     if card.quantity < 1:
-        file = open(fileName, "r")
-        tempFile = open("temp.txt", "w")
-        lineNum = 0
-        name = card.name
-        rpts = 0
         #Searches for card with selected name in database and writes all others to temp file
         for line in file:
-            card = line.split(SEP)
             if line.strip() != name and rpts == 0 or rpts == 11:
                 tempFile.write(line)
             else:
@@ -202,43 +215,101 @@ def delCard():
         #Rewrites temp file back to main file without deleted card
         for line in tempFile:
             file.write(line)
-        tempFile.close()
-        file.close()
         del cards[deckDisplay.curselection()[0]] #Deletes card from list of cards in memory
         deckDisplay.delete(deckDisplay.curselection()[0]) #Deletes selected card from listbox
+    else:
+        #Searches for card with selected name in database and decreases quantity value by 1
+        for line in file:
+            if line.strip() == name:
+                rpts += 1
+            elif rpts > 0:
+                rpts += 1
+            if rpts == 11:
+                tempFile.write(str(int(line) - 1))
+            else:
+                tempFile.write(line)
+        tempFile.close()
+        file.close()
+        file = open(fileName, "w")
+        tempFile = open("temp.txt", "r")
+        for line in tempFile:
+            file.write(line)
+        file.write('\n')
+    tempFile.close()
+    file.close()
+    os.remove('temp.txt')
 
 #Creates new .txt file so user doesn't have to do it manually
 def createDatabase():
     name = simpledialog.askstring(title="Database Name", prompt="Enter the name for the new database:")
     file = open(name + ".txt", "w")
     file.close()
+    file = open('settings.txt', 'w') #writing default settings to file
+    file.write(str(PAGEWAIT) + '\n')
+    file.write(str(SCALE) + '\n')
+    file.close()
 
 #Opens settings menu pop-up
 def openSettings():
     global waitEntry
+    global scaleEntry
     global settings
+    global fontSize
     global PAGEWAIT
+    global SCALE
     settings = tk.Toplevel(main)
-    settings.geometry("400x400")
+    settings.geometry(str(int(round(200 * SCALE))) + 'x' + str(int(round(100 * SCALE))))
     settings.title("Settings")
-    waitLabel = tk.Label(settings, text = 'Max time to let webpages to load').grid(row = 0, column = 0)
-    waitEntry = tk.Entry(settings, width = 3)
+    waitLabel = tk.Label(settings, text = 'Max time to let webpages load', font = fontSize).grid(row = 0, column = 0)
+    scaleLabel = tk.Label(settings, text = 'UI Scale', font = fontSize).grid(row = 2, column = 0)
+    waitEntry = tk.Entry(settings, width = 4, font = fontSize)
     waitEntry.insert(0, PAGEWAIT)
     waitEntry.grid(row = 1, column = 0)
-    settingsSave = tk.Button(settings, text = 'Save', command = saveSettings).grid(row = 2, column = 1)
+    scaleEntry = tk.Entry(settings, width = 4, font = fontSize)
+    scaleEntry.insert(0, SCALE)
+    scaleEntry.grid(row = 3, column = 0)
+    settingSave = tk.Menu(settings)
+    settingSave.add_command(label = "Save Settings", command = saveSettings)
+    settings.config(menu = settingSave)
+    
+def scaling():
+    global fontSize
+    global SCALE
+    global PAGEWAIT
+    main.geometry(str(int(round(1400 * SCALE))) + 'x' + str(int(round(700 * SCALE))))
+    deckDisplay.config(width = int(round(100 * SCALE)), height = int(round(30 * SCALE)))
+    fontSize = font.Font(size = int(round(10 * SCALE)))
+    delButton.config(font = fontSize)
+    addButton.config(font = fontSize)
+    genImageButton.config(font = fontSize)
+    cardLink.config(font = fontSize)
+    lastUpdated.config(font = fontSize)
+    deckLabel.config(font = fontSize)
+    deckDisplay.config(font = fontSize)
+    main.update()
     
 def saveSettings():
     global waitEntry
     global settings
+    global scaleEntry
+    global SCALE
     global PAGEWAIT
     PAGEWAIT = int(waitEntry.get())
+    SCALE = float(scaleEntry.get())
+    file = open('settings.txt', 'w')
+    file.write(str(PAGEWAIT) + '\n')
+    file.write(str(SCALE) + '\n')
+    file.close()
     settings.destroy()
+    scaling()
 
 #Main entry point for program
+loadSettings()
 main = tk.Tk()
 oldDate = tk.StringVar()
-lastUpdated = tk.Label(main, textvariable=oldDate).grid(row = 0, column = 0)
-main.geometry("1400x700") #Will be scaled in future via a setting
+lastUpdated = tk.Label(main, textvariable=oldDate)
+lastUpdated.grid(row = 0, column = 0)
+main.geometry('1400x700')
 main.title("Magic")
 menu = tk.Menu(main)
 #Top menu of program
@@ -247,17 +318,22 @@ menu.add_command(label = "Create New Database", command = createDatabase)
 menu.add_command(label = "Load Database File", command = loadDatabase)
 menu.add_command(label = "Update Database Prices", command = updateDatabase)
 main.config(menu = menu)
-delButton = tk.Button(main, text = "Delete card", command = delCard).grid(row = 4, column = 0)
-addButton = tk.Button(main, text = "Add new card", command = addCard).grid(row = 5, column = 0)
+delButton = tk.Button(main, text = "Delete card", command = delCard)
+delButton.grid(row = 4, column = 0)
+addButton = tk.Button(main, text = "Add new card", command = addCard)
+addButton.grid(row = 5, column = 0)
 global cardLink
 cardLink = tk.Entry(main, width = 50)
 cardLink.insert(0, "Card TCGPlayer URL")
 cardLink.grid(row = 6, column = 0)
-deckLabel = tk.Label(main, text = "Cards:").grid(row = 1, column = 0)
+deckLabel = tk.Label(main, text = "Cards:")
+deckLabel.grid(row = 1, column = 0)
 global deckDisplay
 deckDisplay = tk.Listbox(main, width = 100, height = 30)
 deckDisplay.grid(row = 2, column = 0)
-genImageButton = tk.Button(main, text = "Display card", command = genImage).grid(row = 3, column = 0)
+genImageButton = tk.Button(main, text = "Display card", command = genImage)
+genImageButton.grid(row = 3, column = 0)
 global cards
 cards = []
+scaling()
 main.mainloop()
